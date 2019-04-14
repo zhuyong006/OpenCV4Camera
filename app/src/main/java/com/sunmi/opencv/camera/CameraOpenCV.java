bd0a870978dbd549e2cda406208e6980fe7ee5d4
zhuyong006 <zhuyong_006@163.com>
2019-04-14 16:19:08 +0800

    switch JavaCamera2View , add menu

--- app/src/main/java/com/sunmi/opencv/camera/CameraOpenCV.java
+++ app/src/main/java/com/sunmi/opencv/camera/CameraOpenCV.java
@@ -5,14 +5,23 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
-public class CameraOpenCV extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    private org.opencv.android.JavaCameraView cv_camera = null;
+public class CameraOpenCV extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 , View.OnClickListener{
+    private org.opencv.android.JavaCamera2View cv_camera = null;
+    private int current_camera_idx = CameraBridgeViewBase.CAMERA_ID_BACK;
+    private Button switch_camera = null;
     private static final String TAG = "OpenCV.Jon";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +34,11 @@ public class CameraOpenCV extends AppCompatActivity implements CameraBridgeViewB
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        switch_camera = findViewById(R.id.swich_camera);
+        switch_camera.setOnClickListener(this);
         cv_camera = findViewById(R.id.cv_camera);
         cv_camera.setVisibility(SurfaceView.VISIBLE);
-        cv_camera.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_ANY);
+        cv_camera.setCameraIndex(current_camera_idx);
         cv_camera.enableView();
         cv_camera.setCvCameraViewListener(this);
     }
@@ -36,7 +47,23 @@ public class CameraOpenCV extends AppCompatActivity implements CameraBridgeViewB
     public void onCameraViewStarted(int width, int height) {
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.camera_menu,menu);
+        return true;
+    }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.inverse) {
+            Log.e(TAG,"inverse");
+        }else if(item.getItemId() == R.id.gause_blue) {
+            Log.e(TAG,"gause_blue");
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onCameraViewStopped() {
 
@@ -44,7 +71,33 @@ public class CameraOpenCV extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        //Log.e(TAG,"onCameraFrame");
+//        Mat dst = inputFrame.rgba();
+//        //Log.e(TAG,"orientation : " + this.getRequestedOrientation());
+//
+//        if(current_camera_idx == CameraBridgeViewBase.CAMERA_ID_BACK)
+//            Core.rotate(dst, dst, Core.ROTATE_90_CLOCKWISE);
+//        if(current_camera_idx == CameraBridgeViewBase.CAMERA_ID_FRONT)
+//        {
+//            Core.rotate(dst, dst, Core.ROTATE_90_COUNTERCLOCKWISE);
+//            Core.flip(dst, dst, 1);
+//        }
         return inputFrame.rgba();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.swich_camera)
+        {
+            if(current_camera_idx == CameraBridgeViewBase.CAMERA_ID_BACK){
+                current_camera_idx = CameraBridgeViewBase.CAMERA_ID_FRONT;
+            }else{
+                current_camera_idx = CameraBridgeViewBase.CAMERA_ID_BACK;
+            }
+            //Log.e(TAG,"click");
+            cv_camera.disableView();
+            cv_camera.setCameraIndex(current_camera_idx);
+            cv_camera.enableView();
+
+        }
     }
 }
